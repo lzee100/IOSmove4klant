9ae37cc982e727fe874959ea676e73446c6932c0
Leo <lzee100@gmail.com>
2014-11-25 14:26:06 +0100

    first commit

---
+++ Move4Mobile/LikedCategoriesCell.swift
@@ -0,0 +1,24 @@
+//
+//  LikedCategoriesCell.swift
+//  Move4Klant
+//
+//  Created by Leo van der Zee on 25-11-14.
+//  Copyright (c) 2014 Move4Mobile. All rights reserved.
+//
+
+import UIKit
+
+class LikedCategoriesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
